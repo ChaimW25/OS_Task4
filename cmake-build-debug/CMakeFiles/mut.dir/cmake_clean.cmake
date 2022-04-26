cdd91d5bbd0b61ad50d6d9ec97324c6474aaa169
chaimw <willingerch@gmail.com>
2022-04-26 07:00:16 -0700

    ses

---
+++ cmake-build-debug/CMakeFiles/mut.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mut.dir/mutex.c.o"
+  "mut"
+  "mut.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
